d8bdfb199b79527b636e353d31f74e39a0169a0c
Marcos Brendon <marcos15975321@gmail.com>
2020-09-14 12:37:14 -0300

    add basic handlebars

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const handlebars = require('express-handlebars')
+const path = require('path')
 
 const port = process.env.PORT || 80
 const app = express()
 
+app.engine('handlebars',handlebars({defaultLayout:'basic'}))
+app.set("view engine","handlebars")
+app.use(express.static(path.join(__dirname,"public")))
+
 app.get('/',(req,res)=>{
-    res.send("rodando")
+    res.render("index")
 })
 
 app.listen(port)
